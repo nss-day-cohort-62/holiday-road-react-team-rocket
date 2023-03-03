f275b67ef1495f3beea0c0ebfe1cf21ea71165ff
Jackson Henderson <jbhhenderson@gmail.com>
2023-03-03 15:43:02 -0600

    Mostly fixed the map

--- holiday-road/src/itineraries/SavedItineraryDetails.js
+++ holiday-road/src/itineraries/SavedItineraryDetails.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom"
 import { getAttractionsByIds } from "../providers/AttractionProvider"
 import { getEateriesByIds } from "../providers/EateryProvider"
 import { getSavedItineraryById } from "../providers/ItineraryProvider"
+import { Events } from "../providers/ParkProvider"
 import { getParksByIds } from "../providers/ParkProvider"
 
 export const SavedItineraryDetails = () => {
@@ -10,6 +11,7 @@ export const SavedItineraryDetails = () => {
     const [savedEateries, setSavedEateries] = useState([])
     const [savedAttractions, setSavedAttractions] = useState([])
     const [savedParks, setSavedParks] = useState([])
+    const [detailsDisplay, setDetailsDisplay] = useState(false)
     const { itineraryId } = useParams()
 
     useEffect(
@@ -65,11 +67,65 @@ export const SavedItineraryDetails = () => {
        
     }
 
+    const DetailsButtons = (event, parkObj) => {
+        console.log(parkObj)
+        event.preventDefault()
+        setDetailsDisplay(true)
+        return detailsHTML(parkObj)
+    }
+
+    const detailsHTML = (parkObj) => {
+        Events(parkObj.parkCode).then(
+            (parkEventsArray) => {
+                if (parkEventsArray.data[0]) {
+
+                    return<>
+                    <div>
+                    Event 1:
+                 <p>{parkEventsArray.data[0].title}</p>
+                 <p>{parkEventsArray.data[0].datestart}</p>
+                 <p>{parkEventsArray.data[0].times[0].timestart}</p>
+                 <p>{parkEventsArray.data[0].times[0].timeend}</p>
+                 <p>{parkEventsArray.data[0].description}</p>
+                 <p>{parkEventsArray.data[0].feeinfo}</p>
+                    </div>
+                    </>
+                }
+
+                if (parkEventsArray.data[1]) {
+                    return <>
+                    <div>
+                    Event 1:
+                 <p>{parkEventsArray.data[1].title}</p>
+                 <p>{parkEventsArray.data[1].datestart}</p>
+                 <p>{parkEventsArray.data[1].times[0].timestart}</p>
+                 <p>{parkEventsArray.data[1].times[0].timeend}</p>
+                 <p>{parkEventsArray.data[1].description}</p>
+                 <p>{parkEventsArray.data[1].feeinfo}</p>
+                    </div>
+                    </>
+
+                }
+
+                else{
+                    return <>There are no events scheduled for this park.</>
+                }
+
+            }
+        )
+    }
+
     const DisplayParks = () => {
         if (Array.isArray(savedParks)) {
             return savedParks.map((park) => {
                 return <div className="p-10"> 
                     <h2 className="text-2xl text-center"> {park.fullName}</h2>
+                    <button onClick={ event=>{
+                        
+                        DetailsButtons(event, park)
+
+                    }
+                    }>Details</button>
                     <p className="text-center">{`${park.addresses[0].line1} ${park.addresses[0].city}, ${park.addresses[0].stateCode}`}</p>
                 </div>
             })
@@ -112,6 +168,10 @@ export const SavedItineraryDetails = () => {
             {
                 savedItinerary && foundParks ? DisplayParks()
                     : ""
+            }
+            {
+                detailsDisplay ? detailsHTML()
+                : ""
             }
              <h1 className="text-3xl text-center">Eateries</h1>
             {
--- holiday-road/src/providers/DirectionProvider.js
+++ holiday-road/src/providers/DirectionProvider.js
@@ -1,10 +1,5 @@
-import { MapMarker } from "../directions/DirectionDetails"
-import { getAttractionsByIds } from "./AttractionProvider"
-import { getEateriesByIds } from "./EateryProvider"
-import { getParksByIds } from "./ParkProvider"
-
 export const Geocoding = (city) => {
-    return fetch(`https://graphhopper.com/api/1/geocode?q=${city}&key=7eec464a-0db4-49e0-bba6-6bc40c2b72b6`)
+    return fetch(`https://graphhopper.com/api/1/geocode?q=${city}&limit=1&key=7eec464a-0db4-49e0-bba6-6bc40c2b72b6`)
     .then(response => response.json())
 }
 
@@ -17,24 +12,30 @@ export const LocationHTTPS = (geoCode) => {
 
 export const LocationsMap = (eateriesArray, attractionArray, parksArray) => {
     let geoCodePromises = []
-
-
-    attractionArray.map((attraction) => {
-        geoCodePromises.push(Geocoding(attraction[0]?.city)) 
-    })    
-    eateriesArray.map((eatery) => {
-       geoCodePromises.push(Geocoding(eatery[0]?.city))
-    })
-    parksArray.map((park) => {
-        geoCodePromises.push(Geocoding(park?.addresses[0]?.city))
-     })
+    console.log(eateriesArray)
+    console.log(attractionArray)
+    console.log(parksArray)
+    
+    if(attractionArray) {
+        attractionArray.map((attraction) => {
+            geoCodePromises.push(Geocoding(attraction?.city)) 
+        })    
+    }
+    if(eateriesArray) {
+        eateriesArray.map((eatery) => {
+            geoCodePromises.push(Geocoding(eatery?.city))
+        })
+    }
+    if(parksArray) {
+        parksArray.map((park) => {
+            geoCodePromises.push(Geocoding(park?.addresses?.city))
+         })
+    }
 
     return Promise.all(geoCodePromises).then(
         (geoCodes) => {
-            geoCodes.map((geoCode) => {
-                console.log(geoCode)
-                MapMarker(geoCode)
-            })
+            console.log(geoCodes)
+            return geoCodes
         }
     )
     }
